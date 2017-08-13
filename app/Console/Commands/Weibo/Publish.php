957dfafc2ecd0b67446b65fbd523697fd991246d
Sinute <sinute.lu@gmail.com>
2017-08-14 00:17:28 +0800

    frequency limit

--- app/Console/Commands/Weibo/Publish.php
+++ app/Console/Commands/Weibo/Publish.php
@@ -28,37 +28,49 @@ class Publish extends Command
      */
     public function handle()
     {
+        // fuck weibo sdk
         $_SERVER['REMOTE_ADDR'] = '233.233.233.233';
 
         $weiboAccounts = DB::table('weibo')->get();
         foreach ($weiboAccounts as $weiboAccout) {
+            $this->comment("Start {$weiboAccout->fansub_id} (last from {$weiboAccout->last_id}):");
             $contents = DB::table('publish')
-                ->where('id', '>', $weiboAccout['last_id'])
-                ->where('fansub_id', $weiboAccout['fansub_id'])
+                ->where('id', '>', $weiboAccout->last_id)
+                ->where('fansub_id', $weiboAccout->fansub_id)
                 ->orderBy('publish_time', 'asc')
                 ->get();
-            $lastID      = $weiboAccout['last_id'];
-            $appKey      = $weiboAccout('app_key');
-            $appSecret   = $weiboAccout('app_secret');
-            $accessToken = $weiboAccout('access_token');
+            $count = count($contents);
+            $this->comment("Find {$count} to publish.");
+            $lastID      = $weiboAccout->last_id;
+            $appKey      = $weiboAccout->app_key;
+            $appSecret   = $weiboAccout->app_secret;
+            $accessToken = $weiboAccout->access_token;
             foreach ($contents as $content) {
                 $client = new SaeTClientV2($appKey, $appSecret, $accessToken);
-                if (mb_strlen($content['title']) > 62) {
-                    $title = mb_substr($content['title'], 0, 60) . '..';
+                if (mb_strlen($content->title) > 62) {
+                    $title = mb_substr($content->title, 0, 60) . '..';
                 } else {
-                    $title = $content['title'];
+                    $title = $content->title;
                 }
-                $downloadLink = explode('&', $content['download_link'])[0];
-                $link         = "https://share.dmhy.org{$content['link']}";
+                $downloadLink = explode('&', $content->download_link)[0];
+                $link         = "https://share.dmhy.org{$content->link}";
                 $status       = "{$title} {$downloadLink} {$link}";
-                if ($client->share($status)) {
-                    $lastID = $content['id'];
+                $result       = $client->share($status);
+                if ($result && isset($result['id'])) {
+                    $this->info("Publish [{$content->title}]({$content->id}) success!");
+                    $lastID = $content->id;
                 } else {
+                    $this->error("Publish [{$content->title}]({$content->id}) fail!");
+                    if (isset($result['error'])) {
+                        $this->error("{$result['error']}");
+
+                    }
                     break;
                 }
+                sleep(3);
             }
-            if ($lastID > $weiboAccout['last_id']) {
-                DB::table('weibo')->where('id', $weiboAccout['id'])
+            if ($lastID > $weiboAccout->last_id) {
+                DB::table('weibo')->where('id', $weiboAccout->id)
                     ->update(['last_id' => $lastID]);
             }
         }
